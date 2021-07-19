02cf86683edd40c16d1c5d376f30d289c188805e
noamteyssier <nb.teyssier@gmail.com>
2021-07-19 10:38:09 -0700

    comments

--- src/crispr/matcher.rs
+++ src/crispr/matcher.rs
@@ -1,7 +1,6 @@
 use super::Library;
 use crate::reader::{FastqRead, FastqRecord};
 
-
 pub struct Matcher <B> {
     reader: B,
     library: Library
@@ -10,16 +9,19 @@ impl <B> Matcher <B>
 where
     B: FastqRead + Iterator<Item = FastqRecord>
 {
+    /// Creates a new matching instance
     pub fn new(reader: B, library: Library) -> Self {
         Self {
             reader, library
         }
     }
 
+    /// Runs the matching algorithm
     pub fn run(&mut self) {
         self.library.match_reader(&mut self.reader);
     }
 
+    /// Write summary statistics to file + stdout
     pub fn summary(&mut self, output_filename: &str) {
         self.library.write_count_table(output_filename);
         self.library.summary();
